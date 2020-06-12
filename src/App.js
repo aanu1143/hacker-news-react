3ee1f21b0113fcfff902c44c0c544eb048b6760b
Anand Biswas <anandmaya48@gmail.com>
2020-06-12 22:21:14 +0530

    server side search

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ const PARAM_SEARCH = 'query=';
       };
 
       this.setSearchTopStories = this.setSearchTopStories.bind(this);
+      this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
       this.onSearchChange = this.onSearchChange.bind(this);
+      this.onSearchSubmit = this.onSearchSubmit.bind(this);
       this.onDismiss = this.onDismiss.bind(this);
     }
 
@@ -27,17 +29,27 @@ const PARAM_SEARCH = 'query=';
       this.setState({ result });
     }
 
-      
-    componentDidMount() {
-      const { searchTerm } = this.state;
+
+    fetchSearchTopStories(searchTerm) {
       fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
     }
+      
+    componentDidMount() {
+      const { searchTerm } = this.state;
+      this.fetchSearchTopStories(searchTerm);
+    }
 
     onSearchChange(event) {
       this.setState({ searchTerm: event.target.value });
+    }
+
+    onSearchSubmit(event) {
+      const { searchTerm } = this.state;
+      this.fetchSearchTopStories(searchTerm);
+      event.preventDefault();
     }
 
     onDismiss(id) {
@@ -61,15 +73,18 @@ const PARAM_SEARCH = 'query=';
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
           </div>
-          <Table
-            list={result.hits}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
+          { result
+            && <Table
+              list={result.hits}
+              // pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            />
+          }
         </div>
       );
     }
@@ -77,25 +92,28 @@ const PARAM_SEARCH = 'query=';
 
   class Search extends Component {
     render() {
-      const { value, onChange, children } = this.props;
+      const { value, onChange, onSubmit, children } =this.props;
         return (
-          <form>
-            {children} <input
-              type="text"
-              value={value}
-              onChange={onChange}
+          <form onSubmit={onSubmit}>
+            <input
+            type="text"
+            value={value}
+            onChange={onChange}
             />
+            <button type="submit">
+              {children}
+            </button>
           </form>
         );
       }
-  }
+    }
   
   class Table extends Component {
     render() {
       const { list, pattern, onDismiss } = this.props;
       return (
         <div className="table">
-          {list.filter(isSearched(pattern)).map(item =>
+          {list.map(item =>
             <div key={item.objectID} className="table-row">
               <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
